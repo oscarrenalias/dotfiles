25b834602e68f1885929bdfb99a26449d7f1e489
Oscar Renalias <oscar@renalias.net>
2012-09-15 23:06:45 +0300

    Added some automatic linking as part of the bootstrap process

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+CURFOLDER=`pwd`
+OS=`uname | tr '[A-Z]' '[a-z]'`
+
+echo Current folder is $CURFOLDER
+echo Current OS is $OS
 
 echo Linking this folder to $HOME/.dotfiles
 ln -s `pwd` $HOME/.dotfiles
 
-echo Not implemented yet, you will have to symlink the files manually for now...
+# back things up
+echo Backing things up...
+if [ -f $HOME/.ssh ]; then
+	mv $HOME/.ssh $HOME/.ssh.dotfiles-backup
+fi
+
+if [ -f $HOME/.profile ]; then
+	mv $HOME/.profile $HOME/.profile.dotfiles-backup
+fi
+
+# configure the new links
+echo Installing bash .profile
+ln -s $HOME/.dotfiles/$OS/profile $HOME/.profile
+
+echo Installing SSH files
+ln -s $HOME/.dotfiles/ssh $HOME/.ssh
