9fb80ac5f44ea82778c3318f0c3e4c1ba524ff91
Oscar Renalias <oscar@renalias.net>
2013-02-07 23:10:01 +0200

    Some additions to the emacs config file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,3 +5,9 @@
 (load "puppet-mode-init")
 
 (add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
+
+;; show line number and column number
+(line-number-mode t)                     ;; show line numbers
+(column-number-mode t)                   ;; show column numbers
+(size-indication-mode t)
+(global-linum-mode t)
